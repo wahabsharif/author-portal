1fb670d68cf2bdc96db51de672d9bd54c8282073
Muhammad Wahab Sharif <www.rex19@gmail.com>
2024-09-12 17:53:46 +0500

    Loading Bug Fixed

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -5,6 +5,7 @@ import { useRouter } from "next/navigation";
 
 interface AuthContextType {
   isAuthenticated: boolean;
+  loading: boolean;
   login: (token: string) => void;
   logout: () => void;
 }
@@ -13,32 +14,40 @@ const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [loading, setLoading] = useState(true);
   const router = useRouter();
 
   useEffect(() => {
-    const token = localStorage.getItem("dummyToken");
+    const token = sessionStorage.getItem("token");
+
     if (token) {
       setIsAuthenticated(true);
     } else {
       setIsAuthenticated(false);
     }
+
+    setLoading(false);
   }, []);
 
-  const login = (token: string = "dummyToken") => {
-    localStorage.setItem("dummyToken", token);
+  const login = (token: string) => {
+    sessionStorage.setItem("token", token);
     setIsAuthenticated(true);
     router.push("/");
   };
 
-  const logout = () => {
-    localStorage.removeItem("dummyToken");
-    setIsAuthenticated(false);
-    router.push("/auth");
+  const logout = async () => {
+    try {
+      sessionStorage.removeItem("token");
+      setIsAuthenticated(false);
+      router.push("/auth");
+    } catch (error) {
+      console.error("Failed to log out:", error);
+    }
   };
 
   return (
-    <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
-      {children}
+    <AuthContext.Provider value={{ isAuthenticated, loading, login, logout }}>
+      {loading ? null : children}
     </AuthContext.Provider>
   );
 };
